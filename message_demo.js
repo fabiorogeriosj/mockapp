99dc83dcdfc829fbdb2f1d92bdf331b0c1bccd1e
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2016-10-01 17:11:08 -0300

    test

--- message_demo.js
+++ message_demo.js
@@ -1,14 +1,15 @@
-var msg = {}
-msg.en += "\n                                       ";
-msg.en  = "\n  Hey, nice?! This is example message  ";
-msg.en += "\n  from repository for all users, this  ";
-msg.en += "\n  will show after run command success  ";
-msg.en += "\n                                       ";
+{
+  "en" : "\n                                       " +
+         "\n  Hey, nice?! This is example message  " +
+         "\n  from repository for all users, this  " +
+         "\n  will show after run command success  " +
+         "\n                                       ",
 
 
-msg.pt += "\n                                    ";
-msg.pt  = "\n  Show, blz?! Este é um exemplo de  ";
-msg.pt += "\n  mensagem vinda do servidor, isto  ";
-msg.pt += "\n  só será exibido quando executar   ";
-msg.pt += "\n  um comando com sucesso            ";
-msg.pt += "\n                                    ";
+  "pt" : "\n                                    " +
+         "\n  Show, blz?! Este é um exemplo de  " +
+         "\n  mensagem vinda do servidor, isto  " +
+         "\n  só será exibido quando executar   " +
+         "\n  um comando com sucesso            " +
+         "\n                                    ",
+}

b2f73e8ce780abed04651f10feb9a1ae237c3490
Fabio <fabio.rogerio.sj@gmail.com>
2016-10-21 21:07:17 -0200

    fix test console

--- test/test_command_install_service.js
+++ test/test_command_install_service.js
@@ -15,11 +15,11 @@ describe('#install service camera', function() {
   before(function (done) {
       var installServiceCommand = spawn('node', ['../bin/mockapp','install','service',SERVICE_TEST,'--lang','en'], OPTIONS);
       installServiceCommand.stdout.on('data', function (data) {
-        captured_stdout += data.toString();
+        captured_stdout = data.toString();
       });
 
       installServiceCommand.stderr.on('data', function (data) {
-        captured_stdout += data.toString();
+        captured_stdout = data.toString();
       });
 
       installServiceCommand.on('exit', function (code) {
